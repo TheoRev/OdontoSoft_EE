a9dc9b5ff5bf3fd78e88b52f8758e95fe3415120
TheoRev <hrevillafernandez79@gmail.com>
2017-12-12 01:21:28 -0500

    Iniciando el crud de pacientes

--- src/java/com/theorev/controller/PatientsController.java
+++ src/java/com/theorev/controller/PatientsController.java
@@ -12,63 +12,74 @@ import javax.faces.view.ViewScoped;
 import com.theorev.dao.PatientsDao;
 import com.theorev.model.Patients;
 import com.theorev.service.IOdontoSoft;
+import com.theorev.util.AccionUtil;
 import com.theorev.util.MessagesUtil;
 
 @ManagedBean
 @ViewScoped
 public class PatientsController implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private List<Patients> pacientes;
-	private Patients paciente;
+    private List<Patients> pacientes;
+    private Patients paciente;
 
-	private String accion;
+    private String accion;
 
-	@PostConstruct
-	public void init() {
-		paciente = new Patients();
-		findAll();
-	}
+    @PostConstruct
+    public void init() {
+        paciente = new Patients();
+        findAll();
+    }
 
-	public void findAll() {
-		FacesMessage message = null;
+    public void findAll() {
+        FacesMessage message = null;
 
-		IOdontoSoft<Patients> dao = new PatientsDao();
+        IOdontoSoft<Patients> dao = new PatientsDao();
 
-		try {
-			pacientes = dao.findAll("Patients", "", "");
-		} catch (Exception e) {
-			message = new FacesMessage(FacesMessage.SEVERITY_ERROR, MessagesUtil.ERROR_DB_TITLE, e.getMessage());
-		}
+        try {
+            pacientes = dao.findAll("Patients", "", "");
+        } catch (Exception e) {
+            message = new FacesMessage(FacesMessage.SEVERITY_ERROR, MessagesUtil.ERROR_DB_TITLE, e.getMessage());
+        }
 
-		if (message != null) {
-			FacesContext.getCurrentInstance().addMessage(null, message);
-		}
-	}
+        if (message != null) {
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
 
-	public List<Patients> getPacientes() {
-		return pacientes;
-	}
+    public void doNew() {
+        accion = AccionUtil.CREATE;
+        paciente = new Patients();
+    }
 
-	public void setPacientes(List<Patients> pacientes) {
-		this.pacientes = pacientes;
-	}
+    public void doModify(Patients p) {
+        accion = AccionUtil.UPDATE;
+        paciente = p;
+    }
 
-	public Patients getPaciente() {
-		return paciente;
-	}
+    public List<Patients> getPacientes() {
+        return pacientes;
+    }
 
-	public void setPaciente(Patients paciente) {
-		this.paciente = paciente;
-	}
+    public void setPacientes(List<Patients> pacientes) {
+        this.pacientes = pacientes;
+    }
 
-	public String getAccion() {
-		return accion;
-	}
+    public Patients getPaciente() {
+        return paciente;
+    }
 
-	public void setAccion(String accion) {
-		this.accion = accion;
-	}
+    public void setPaciente(Patients paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getAccion() {
+        return accion;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
 
 }
--- src/java/com/theorev/util/AccionUtil.java
+++ src/java/com/theorev/util/AccionUtil.java
@@ -4,5 +4,5 @@ public class AccionUtil {
 
     public static final String CREATE = "NUEVO";
     public static final String NUEVA = "NUEVA";
-    public static final String UPDATE = "ACTUALIZAR";
+    public static final String UPDATE = "MODIFICAR";
 }
